2684e9c5412e03b4c24f57b0722f8962669f731a
Hima-Kumi <125667773+Hima-Kumi@users.noreply.github.com>
2026-03-25 11:51:40 +0900

    Add files via upload

--- js/loyalty_new.js
+++ js/loyalty_new.js
@@ -7,7 +7,7 @@
   // ============================================================
   // Configuration
   // ============================================================
-  const PUBLIC_API_URL = "https://script.google.com/macros/s/AKfycbxngYlwl6HDcXcJ35m2ic46jDuYGyGUGCRZbOO7vjk486rGMSFepu6OCIg07PKmrDk4/exec";
+  const PUBLIC_API_URL = "https://script.google.com/macros/s/AKfycbz3jAxjFwED1yzQgS0KsCd6LHzh5hxnnzxGm9_B3TnkFHngvOnp_uTMdEDkFkCLhMU4/exec";
   const SITE_KEY = "model-a";
   const LS_DEVICE_ID_KEY = "loyaltyDeviceId";
   const LS_USER_ID_KEY = "loyaltyUserId";
